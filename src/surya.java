11a2cc5ec1371aa17146ecf88a8b998599d1c7b2
Suryaprakash-Shankar <surya055cs@gmail.com>
2025-06-25 11:10:42 +0530

    added a new file to test

---
+++ src/surya.java
@@ -0,0 +1,17 @@
+public class surya{
+    public String Name = "Surya";
+    public String Age = "20";
+
+    public void details() {
+        System.out.println("Name :" + Name);
+        System.out.println("Age :" + Age);
+    }
+    public static void main(String args[]){
+
+        surya s = new surya();
+        s.details();
+
+
+    }
+
+}
