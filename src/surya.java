7ac2aa790f78021f6170e82442b6ea2b87040fec
Suryaprakash-Shankar <surya055cs@gmail.com>
2025-06-25 12:21:10 +0530

    Add methods in java file

--- src/surya.java
+++ src/surya.java
@@ -1,17 +1,36 @@
+//public class surya{
+//    public String Name = "Surya";
+//    public String Age = "20";
+//
+//    public void details() {
+//        System.out.println("Name :" + Name);
+//        System.out.println("Age :" + Age);
+//    }
+//    public static void main(String args[]){
+//
+//        surya s = new surya();
+//        s.details();
+//
+//
+//    }
+//
+//}
+
 public class surya{
-    public String Name = "Surya";
-    public String Age = "20";
+    public String name;
+    public int age;
+public  surya(String name,int age){
+    this.name = name;
+    this.age = age;
+}
 
-    public void details() {
-        System.out.println("Name :" + Name);
-        System.out.println("Age :" + Age);
-    }
-    public static void main(String args[]){
+public void display(){
+    System.out.println("Name : " + name);
+    System.out.println("Age : " + age);
+}
 
-        surya s = new surya();
-        s.details();
-
-
-    }
-
+public static void main(String args[]){
+    surya s1 = new surya("surya",20);
+    s1.display();
+}
 }
